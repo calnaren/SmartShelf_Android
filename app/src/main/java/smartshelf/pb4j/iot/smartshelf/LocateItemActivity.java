84dc7869b4fcddc2219e7e68ad9b3d0eb7e2cdf4
calnaren <naren.vasanad@berkeley.edu>
2015-04-22 23:40:32 -0700

    Added list view to the view item list
    This is static for now. Needs to be changed to update as and when an item has been scanned.

--- app/src/main/java/smartshelf/pb4j/iot/smartshelf/LocateItemActivity.java
+++ app/src/main/java/smartshelf/pb4j/iot/smartshelf/LocateItemActivity.java
@@ -1,9 +1,19 @@
 package smartshelf.pb4j.iot.smartshelf;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class LocateItemActivity extends ActionBarActivity {
@@ -12,8 +22,57 @@ public class LocateItemActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locate_item);
+
+        final ListView listview = (ListView) findViewById(R.id.itemList);
+        String[] values = new String[]{"Ibuprofen", "Murinae", "Graval",
+                "Hydrocodone", "Zocor", "Lisinopril", "Norvasc", "Azithromycin",
+                "Amoxicillin"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                Intent intent = new Intent(LocateItemActivity.this, ViewShelfActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
